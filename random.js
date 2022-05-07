cfa90359e34525963b92e2e689745d3fd672c8f0
MdPavelSarker <pavelsarkergithub@gmail.com>
2022-05-08 00:32:37 +0600

    randomNumber

---
+++ random.js
@@ -0,0 +1,13 @@
+var num = 2.135;
+var result  = Math.floor(num);
+var result2  = Math.ceil(num);
+var result3  = Math.round(num);
+for(var i = 0; i<10; i++){
+    var randomNumber = Math.random() * 6;
+    var output = Math.round(randomNumber);
+    console.log(output);
+}
+
+    // var randomNumber = Math.random() * 6;
+    // var output = Math.round(randomNumber);
+    // console.log(output);
